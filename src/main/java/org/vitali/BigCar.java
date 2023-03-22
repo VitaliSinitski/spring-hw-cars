f9d628566a0f1e6dc2bafbc9ed5efb646c169001
Vitali Sinitski <vitali.sin777@gmail.com>
2023-03-22 23:19:47 +0300

    1.1

---
+++ src/main/java/org/vitali/BigCar.java
@@ -0,0 +1,8 @@
+package org.vitali;
+
+public class BigCar implements ICar{
+    @Override
+    public String getDrive() {
+        return "drive bigCar";
+    }
+}
---
+++ src/main/java/org/vitali/Car.java
@@ -0,0 +1,8 @@
+package org.vitali;
+
+public class Car implements ICar{
+    @Override
+    public String getDrive() {
+        return "drive car";
+    }
+}
